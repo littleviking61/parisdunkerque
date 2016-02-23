2179e41664b6187ef888cb7ead018c8c6e31ea69
Baptiste Régné <baptiste.regne@gmail.com>
2016-02-23 17:00:51 +0100

    add pattern plus header good width

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,9 @@
 
 		var sticky = new Waypoint.Sticky({
 		  element: $('header.header')[0]
-		})
+		});
+
+		var header = $('header');
 
 		$('a[rel="nofollow"], li.top > a','.header nav').click(function(e){
 
@@ -16,7 +18,10 @@
 				var section = $(this).data('title');
 				var url = '#'+section;
 				if($(this).parent('li').hasClass('top')) $(window).scrollTo(0, 400);
-				else $(window).scrollTo('#'+section, 400);
+				else {
+					var position = $('#'+section).offset().top - header.outerHeight();
+					$(window).scrollTo(position, 400);
+				}
 
 				history.replaceState({section: '#'+section}, 'Section' + section, url);
 			}else{
